00e7758ab2ea8787205fec31dacda12036bb92f0
uki00a <uki00a@gmail.com>
2020-10-11 21:57:54 +0900

    chore: use faker.js for tests (#2)

--- sender_test.ts
+++ sender_test.ts
@@ -1,23 +1,24 @@
-import { assertEquals } from "./test_deps.ts";
+import { assertEquals, faker } from "./test_deps.ts";
 import { suite, waitFor } from "./test_utils.ts";
 import { createFluentSender } from "./sender.ts";
 
 suite("sender", ({ test, todo, server }) => {
   test("simple", async () => {
-    const tagPrefix = "foo";
+    const tagPrefix = faker.random.word();
     const sender = createFluentSender({
       tagPrefix,
       port: server.port,
     });
     try {
-      const tag = "bar";
-      await sender.post(tag, { "name": "bob" });
+      const tag = faker.random.word();
+      const event = { "name": faker.name.findName() };
+      await sender.post(tag, { ...event });
       await waitFor(() => {
         const data = server.getRecievedData();
         assertEquals(data.length, 1);
-        assertEquals(data[0][0], "foo.bar");
+        assertEquals(data[0][0], `${tagPrefix}.${tag}`);
         assertEquals(typeof data[0][1], "number");
-        assertEquals(data[0][2], { "name": "bob" });
+        assertEquals(data[0][2], event);
       });
     } finally {
       sender.close();
--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,3 @@
 export * from "https://deno.land/std@0.71.0/testing/asserts.ts";
 export { deferred } from "https://deno.land/std@0.71.0/async/deferred.ts";
+export { default as faker } from "https://cdn.skypack.dev/faker@^4.1.0";
